47f311544295b7338d69056c11b76f2d0e21fea7
Ronakpiraka <78247891+Ronakpiraka@users.noreply.github.com>
2021-06-13 02:03:23 +0530

    Create youtube_downloader.py

---
+++ youtube_downloader.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+link = input("Enter your youtube url: ")
+yt = YouTube(link)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+for i in video:
+    print(i)
+
+print("Enter the desired option to download the format")
+dn_option = int(input("enter the option: "))
+dn_video = video[dn_option]
+dn_video.download()
+
+print("downloaded successfully")
